e5c91df715171b4ab25eb25c1bef5b75860b8140
Dharmesh Jogadia <dharmeshjogadia@gmail.com>
2014-02-03 22:05:17 +0530

    Successfully Submitted

---
+++ ProjARMY.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int n,t,lNG,lNM,maxNG,maxNM;
+    scanf("%d",&t);
+
+    while(t--)
+    {
+        printf("\n");
+        scanf("%d %d",&lNG,&lNM);
+        maxNG=0;
+        maxNM=0;
+        while(lNG--)
+        {
+            scanf("%d",&n);
+            if(maxNG<n)
+                maxNG=n;
+
+        }
+
+        while(lNM--)
+        {
+            scanf("%d",&n);
+            if(maxNM<n)
+                maxNM=n;
+
+        }
+        if(maxNG>=maxNM)
+            printf("Godzilla\n");
+        else
+            printf("MechaGodzilla\n");
+    }
+    return 0;
+}
+
