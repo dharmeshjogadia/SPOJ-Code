3cc98d8ae62b0e9dc5e1cd934ba7b47f19afc2bc
Dharmesh Jogadia <dharmeshjogadia@gmail.com>
2014-02-23 21:39:46 +0530

    Successfully Submmited

---
+++ Candy I.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+        int n,c,i;
+        long tmp;
+        long s;
+        while(1)
+        {
+            cin>>n;
+
+            if(n==-1)
+                break;
+            int v[n];
+            s=0;
+            for(i=0;i<n;i++)
+            {
+                cin>>v[i];
+                s+=v[i];
+            }
+            tmp=s/n;
+            c=0;
+            if(tmp*n==s)
+            {
+                for(i=0;i<n;i++)
+                {
+                    if(tmp>v[i])
+                    c+=tmp-v[i];
+                }
+                cout<<c<<"\n";
+            }
+            else
+                cout<<"-1\n";
+        }
+}
