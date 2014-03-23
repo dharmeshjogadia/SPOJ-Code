c9ddada6f6088813d5ffca976515fdf962aa8e16
Dharmesh Jogadia <dharmeshjogadia@gmail.com>
2014-03-23 22:48:42 +0530

    AC

---
+++ Eat all the brownies !.cpp
@@ -0,0 +1,26 @@
+/*
+SPOJ Problem Set (classical)
+
+17626. Eat all the brownies !
+
+Problem code: CUTCAKE
+*/
+#include<stdio.h>
+int main()
+{
+	long long t,n,ps,cut;
+	scanf("%lld",&t);
+	while(t--)
+	{
+		ps=1;
+		cut=0;
+		scanf("%lld",&n);	
+		while(ps<n)
+		{
+			cut++;
+			ps+=cut;
+		}
+		printf("%lld\n",cut);
+	}
+return 0;
+}
